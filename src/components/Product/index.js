aa09eaecb7e5020a9b0e62a6ff9603263db23408
Awais Hussain <awais.hussain@covvi.com>
2021-12-20 15:15:31 +0000

    Added basic animation

--- src/components/Product/index.js
+++ src/components/Product/index.js
@@ -5,13 +5,35 @@ import {
     StyleSheet, 
     Dimensions, 
     ImageBackground, 
-    Image 
+    Image,
+    Animated,
+    TouchableWithoutFeedback,
+    Easing
 } from "react-native";
 import {getDisplayPrice} from "../../library/ProductsHelper"
 
 const width = Dimensions.get('window').width * 0.8;
 
 const Product = ({product}) => {
+    const animatedValue = new Animated.Value(0)
+
+    const onHold = () => {
+        Animated.timing(animatedValue, {
+            toValue: 0.005,
+            duration: 100,
+            easing: Easing.ease
+        }).start()
+    }
+
+    const onRelease = () => {
+        Animated.timing(animatedValue, {
+            toValue: 0,
+            duration: 100,
+            easing: Easing.ease
+        }).start()
+
+        // Navigate to Details Screen
+    }
 
     const DisplayPrices = () => {
         const {price, discountedPrice} = getDisplayPrice(product)
@@ -24,46 +46,66 @@ const Product = ({product}) => {
             <Text style={styles.boldLabel}>{price}</Text>
     }
 
+    const transform = {transform: [
+        {
+            scaleX: animatedValue.interpolate({
+                inputRange: [0, 1],
+                outputRange: [1, 15]
+            })
+        },
+        {
+            scaleY: animatedValue.interpolate({
+                inputRange: [0, 1],
+                outputRange: [1, 12.5]
+            })
+        }
+    ]}
+
     return (
-        <View style={styles.container}>
-            <ImageBackground
-                style={styles.backgroundImage}
-                source={{
-                    uri: product.image
-                }}
-                resizeMode='cover'
-            >
-                <View style={styles.detailsView}>
-                    <View 
-                        style={{flex: 1}}
-                    >
-                        <Text 
-                            style={styles.boldLabel} 
-                            numberOfLines={1} 
-                            ellipsizeMode='tail'
+        <TouchableWithoutFeedback 
+            onPressIn={onHold} 
+            onPressOut={onRelease}
+        >
+            <Animated.View style={[styles.container, transform]}>
+                <ImageBackground
+                    style={styles.backgroundImage}
+                    source={{
+                        uri: product.image
+                    }}
+                    resizeMode='cover'
+                >
+                    <View style={styles.detailsView}>
+                        <View 
+                            style={{flex: 1}}
                         >
-                                {product.name}
-                        </Text>
-                        <DisplayPrices />
-                        <Text 
-                            style={styles.productDescriptionLabel} 
-                            numberOfLines={2} 
-                            ellipsizeMode='tail'
+                            <Text 
+                                style={styles.boldLabel} 
+                                numberOfLines={1} 
+                                ellipsizeMode='tail'
+                            >
+                                    {product.name}
+                            </Text>
+                            <DisplayPrices />
+                            <Text 
+                                style={styles.productDescriptionLabel} 
+                                numberOfLines={2} 
+                                ellipsizeMode='tail'
+                            >
+                                {product.short_description}
+                            </Text>
+                        </View>
+                        <View 
+                            style={styles.profileView}
                         >
-                            {product.short_description}
-                        </Text>
+                            <Image 
+                                style={styles.profileImage} 
+                                source={require("../../assets/images/profile.jpg")} 
+                            />
+                        </View>
                     </View>
-                    <View 
-                        style={styles.profileView}
-                    >
-                        <Image 
-                            style={styles.profileImage} 
-                            source={require("../../assets/images/profile.jpg")} 
-                        />
-                    </View>
-                </View>
-            </ImageBackground>
-        </View>
+                </ImageBackground>
+            </Animated.View>
+        </TouchableWithoutFeedback>
     )
 };
 
